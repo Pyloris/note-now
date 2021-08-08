6ca8fc87bcfff54144520767c7ab923757ca525e
pyloris <wanishoaib@protonmail.com>
2021-08-08 19:23:01 +0530

    fixed toggler icon problem

--- website/auth.py
+++ website/auth.py
@@ -63,7 +63,3 @@ def sign_up():
 			return redirect(url_for('views.home'))
 
 	return render_template("sign_up.html", user=current_user)
-
-# i dont think any thing is wrong.
-# my be its browsers fault 
-# wait a sec
